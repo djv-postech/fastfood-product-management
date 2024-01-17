d7cd9171552412cd2ea8a2b8f01161c8b1012312
Jackson Carlos Leite Ramalho <soat.postech@gmail.com>
2024-01-16 21:41:19 -0300

    ajuste k8s artefatos

--- fastfood-product-management/src/test/java/com/fiap/postech/techchallenge/fastfoodproductmanagement/core/domain/usecases/produto/ProdutoHelper.java
+++ fastfood-product-management/src/test/java/com/fiap/postech/techchallenge/fastfoodproductmanagement/core/domain/usecases/produto/ProdutoHelper.java
@@ -6,7 +6,6 @@ import com.fiap.postech.techchallenge.fastfoodproductmanagement.application.api.
 import com.fiap.postech.techchallenge.fastfoodproductmanagement.application.api.produto.records.DadosCadastroProduto;
 import com.fiap.postech.techchallenge.fastfoodproductmanagement.core.domain.entities.produto.Categoria;
 import com.fiap.postech.techchallenge.fastfoodproductmanagement.core.domain.entities.produto.Produto;
-import com.fiap.postech.techchallenge.fastfoodproductmanagement.infra.persistence.repository.entity.ProdutoEntity;
 
 import java.math.BigDecimal;
 
